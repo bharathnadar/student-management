1b360f7cf203b0c21879e94eb6573744e6716d3b
bharathnadar <mailbharath.889@gmail.com>
2024-12-25 10:42:30 +0530

    publish directory

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // Specify the output directory for production build
+    outDir: 'dist', // Ensure this matches the expected publish directory
   },
 })
